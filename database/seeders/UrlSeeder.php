19cc07e1b65381b67fcc4dee579a2a2754e89eba
AslanAV <autlevaslan@yandex.ru>
2022-08-29 14:06:33 +0300

    add UrlSeeder.php

---
+++ database/seeders/UrlSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Url;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UrlSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tableUrls = [
+            'https://hexlet.io',
+            'https://code-basics.com',
+            'https://todoist.com',
+            'https://laravel.com',
+            'https://example.com',
+            'https://symfony.com',
+            'https://ubuntu.com/',
+            'https://ubuntu.com/',
+            'https://www.spacex.com',
+            'https://hubblesite.org',
+            'https://www.nasa.gov',
+            'https://jwst.nasa.gov',
+            'https://www.coca-cola.ru',
+            'https://ru.jbl.com',
+            'https://www.citilink.ru',
+            'https://www.dns-shop.ru',
+            'https://bashorg.org',
+        ];
+
+        foreach ($tableUrls as $key => $url) {
+            DB::table('urls')->insert([
+                'id' => $key + 1,
+                'name' => $url,
+                'created_at' => now(),
+            ]);
+        }
+    }
+}
