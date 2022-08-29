7a676b042f8618f3ecb5e43ab4f1600623fde020
AslanAV <autlevaslan@yandex.ru>
2022-08-29 14:20:13 +0300

    add resource routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UrlController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::view('/','main')->name('home');
+Route::resource('urls', UrlController::class);
+
