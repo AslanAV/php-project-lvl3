8c270021120e3264032e6e66cf920971738df1e8
AslanAV <autlevaslan@yandex.ru>
2022-09-17 23:44:09 +0300

    refactoring 3: rename testStore and fix setUp

--- tests/Feature/UrlChecksTest.php
+++ tests/Feature/UrlChecksTest.php
@@ -14,34 +14,34 @@ class UrlChecksTest extends TestCase
     use RefreshDatabase;
 
     private string $url = '';
-
-    private string $html = '';
+    private array $id = [];
 
     public function setUp(): void
     {
         parent::setUp();
-        $path = __DIR__ . '/../Fixtures/index.html';
-        $content = file_get_contents($path);
-        if ($content === false) {
-            throw new Exception("file path: {$path} - is incorrect");
-        }
-        $this->html = $content;
-        $this->url = 'https://www.azsgnk.ru';
-    }
 
-    public function testCheckUrlSuccess(): void
-    {
-        Http::fake([
-            $this->url => Http::response(
-                $this->html,
-            )
-        ]);
+        $this->url = 'https://www.azsgnk.ru';
 
         $id = DB::table('urls')->insertGetId([
             'name' => $this->url,
             'created_at' => Carbon::now(),
         ]);
+        $this->id = [$id];
+    }
 
+    public function testStore(): void
+    {
+        $path = __DIR__ . '/../Fixtures/index.html';
+        $html = file_get_contents($path);
+        if ($html === false) {
+            throw new Exception("file path: {$path} - is incorrect");
+        }
+
+        Http::fake([
+            $this->url => Http::response($html, 200)
+        ]);
+
+        $id =head($this->id);
         $checkData = [
             'url_id' => $id,
             'status_code' => 200,
@@ -51,7 +51,7 @@ class UrlChecksTest extends TestCase
             'created_at' => Carbon::now(),
         ];
 
-        $checkResponse = $this->post(route('urls.checks.store', [$id]));
+        $checkResponse = $this->post(route('urls.checks.store', $this->id));
         $checkResponse->assertRedirect()->assertStatus(302);
         $checkResponse->assertSessionHasNoErrors();
         $this->assertDatabaseHas('url_checks', $checkData);
